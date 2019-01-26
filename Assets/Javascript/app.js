bc9b85a57443b9ff0e5d92b76c642c6fb35f15a5
Kyle Robert <notoriouskro@gmail.com>
2019-01-26 15:02:25 -0800

    onClick

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -4,6 +4,8 @@ var simpsonsChar = ["Homer Simpson", "Bart Simpson", "Marge Simpson", "Lisa Simp
 // displayGif function re-renders the HTML to display the appropriate content
 function displayGif() {
 
+  $('#simpsonGif').html('');
+
   var sChar = $(this).text();
   var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ sChar +"&api_key=O74jZQvUg3FpFO1X8fI5dC4mP96jCpO6&rating=pg-13&limit=10";
 
@@ -15,19 +17,43 @@ function displayGif() {
   }).then(function(response) {
     console.log(response);
        
-       for (var i = 0;i < queryURL.length;i++){
+       for (var i = 0;i < response.data.length;i++){
         
-        $('#simpsonTitle').append(response.data[i].title);
-        $('#simpsonRating').text(response.data[i].rating);
+        $('#simpsonGif').append("<p><strong>Title:</strong> " + response.data[i].title.charAt(0).toUpperCase() + response.data[i].title.substring(1) + "</p>");
+        
+        $('#simpsonGif').append("<p><strong>Rating:</strong> " + response.data[i].rating.toUpperCase()+ "</p>");
+        
+        var imgUrl = response.data[i].images.downsized_still.url;
+        var gifUrl = response.data[i].images.downsized.url;
         var img = $('<img>');
-        var imgUrl = response.data[i].images.downsized.url;
+        img.attr('src', gifUrl);
+        img.attr('data-still', imgUrl);
+        img.attr('data-animate', gifUrl);
+        img.attr('data-state', 'still');
+        img.attr('class', 'gif');
+
+        var imgUrl = response.data[i].images.downsized_still.url;
         img.attr({src: imgUrl, width: '250px', height: '250px'});
         $('#simpsonGif').append(img);
-       
-      }
+        var br = $('<br />');
+        $('#simpsonGif').append(br);
+        
       
+      }
+      $('.gif').on("click", function() {
+        console.log('Gif Click');
+     var state = $(this).attr('data-state');
+     console.log(state);
+      if (state === 'still') {
+        $(this).attr('src',$(this).attr('data-animate'));
+        $(this).attr('data-state', 'animate');
+        
+      }else {
+        $(this).attr('src',$(this).attr('data-still'));
+        $(this).attr('data-state', 'still');
+      }
 
-
+    })
    
   });
 
@@ -42,7 +68,7 @@ function displayGif() {
   $('#simpson-div').html('');
   // Loop through the array of movies, then generate buttons for each movie in the array
   for (var i = 0; i < simpsonsChar.length; i++) {
-    var button = $('<button>');
+    var button = $('<button class="btn btn-primary">');
     button.text(simpsonsChar[i]);
     button.addClass('searchButton');
     $('#simpson-div').append(button);
